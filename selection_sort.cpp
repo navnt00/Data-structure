8a6b925901fc2e05f621e433291af2a0a21881f0
navnt00 <105013664+navnt00@users.noreply.github.com>
2022-12-11 23:46:47 +0530

    selection_sort

---
+++ selection_sort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int array[8]={2,8,9,5,6,1};
+    for(int i=0 ; i < n-1 ;i++)
+    {
+        int minindex=i;
+
+        for(int j=i+1;j<n;j++){
+            if(array[j]<array[minindex])
+            minindex=j;
+        }
+        swap(array[minindex],array[i]);
+    }
+    cout<<"sorted array is: "<<endl;
+
+
+    return 0;
+}
