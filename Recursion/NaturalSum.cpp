254fa2a24ca00a33ea9740e3560028c608526b1c
navneet yadav <105013664+navnt00@users.noreply.github.com>
2024-08-02 14:24:01 +0530

    backet change

--- Recursion/NaturalSum.cpp
+++ Recursion/NaturalSum.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 using namespace std;
 int naturalSum(int n){
-        if(n <= 1)
+        if(n <= 1){
             return 1;
+        }
         return n + naturalSum(n - 1);     
 }
 int main(){
