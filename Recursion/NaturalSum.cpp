c5b1896d26a63a1f6b4e725b33832fdc695b1c17
navneet yadav <105013664+navnt00@users.noreply.github.com>
2024-08-02 14:12:47 +0530

    add all natural number

---
+++ Recursion/NaturalSum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int naturalSum(int n){
+        if(n <= 1)
+            return 1;
+        return n + naturalSum(n - 1);     
+}
+int main(){
+    int n = 5;
+    cout << naturalSum(n);
+    return 0;
+}
