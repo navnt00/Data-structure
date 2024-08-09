31dfce326dbed4a4a37d3250e934db872a99ca04
navneet yadav <105013664+navnt00@users.noreply.github.com>
2024-08-09 15:05:56 +0530

    wave changed

--- 2Darray/wavematrix.cpp
+++ 2Darray/wavematrix.cpp
@@ -17,13 +17,13 @@ int main(){
     cout << endl;
 
     for (int i = 0; i < m; i++){
-        if(i % 2 == 0){
-            for (int j = 0; j < n; j++){
+        if(i%2 == 0){
+            for (int j = n-1; j >= 0; j--){
                 cout << arr[i][j] << " ";
             }
         }
         else{
-            for (int j = n - 1; j >= 0; j--){
+            for (int j = 0; j < n; j++){
                 cout << arr[i][j] << " ";
             }
         }
